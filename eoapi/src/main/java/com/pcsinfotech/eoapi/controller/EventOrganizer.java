31bb6daa706b2e3f7ef4c4d1e8ce3e7253f55233
ymohan-pcs <ymohan@pcsinfotech.in>
2024-09-10 01:47:23 +0530

    Adding all files

--- eoapi/src/main/java/com/pcsinfotech/eoapi/controller/EventOrganizer.java
+++ eoapi/src/main/java/com/pcsinfotech/eoapi/controller/EventOrganizer.java
@@ -1,16 +1,32 @@
 package com.pcsinfotech.eoapi.controller;
 
+import org.apache.catalina.webresources.TomcatURLStreamHandlerFactory;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ImportResource;
 
-@SpringBootApplication
-@ImportResource("classpath:spring-eoapi.xml")
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@ImportResource("classpath*:spring-eoapi.xml")
 public class EventOrganizer {
 	
 	public static void main(String[] args) {
+		/*
+		 * TomcatURLStreamHandlerFactory.disable(); SpringApplication app = new
+		 * SpringApplication(EventOrganizer.class);
+		 * app.setWebApplicationType(WebApplicationType.SERVLET); app.run(args);
+		 */
 		ConfigurableApplicationContext run = SpringApplication.run(EventOrganizer.class, args);
+	}
+	
+	@Bean
+	public WebServerFactoryCustomizer<ConfigurableServletWebServerFactory> webServerFactoryCustomizer() {
+	    return factory -> factory.setContextPath("/eventorganizer");
 	}
 
 }
